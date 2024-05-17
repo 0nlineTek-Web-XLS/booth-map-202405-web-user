c60201964acf5b08e8d9cbbf6c94386d892e280e
lnkkerst <lnkkerst04@gmail.com>
2024-05-17 16:10:06 +0800

    chore: remove unused deps

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import "reset-css";
 import "@unocss/reset/tailwind.css";
 import "./assets/main.css";
 import "element-plus/dist/index.css";
-import TDesign from "tdesign-mobile-vue";
-import "tdesign-mobile-vue/es/style/index.css";
 import "animate.css";
 import "uno.css";
 
@@ -14,6 +12,5 @@ import App from "./App.vue";
 const app = createApp(App);
 
 app.use(ElementPlus);
-app.use(TDesign);
 
 app.mount("#app");
